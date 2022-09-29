91bd22cfcb0c481f0722eed111cecdf85a574d42
Yuzu <1581523609@qq.com>
2022-09-29 14:05:04 +0800

    1

--- src/views/dashboard/DashboardIndex.vue
+++ src/views/dashboard/DashboardIndex.vue
@@ -2,6 +2,14 @@
   <div>DashboardIndex</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onActivated, onUnmounted } from "vue";
+onActivated(() => {
+  console.log("缓存页打开");
+});
+onUnmounted(() => {
+  console.log("被销毁");
+});
+</script>
 
 <style scoped></style>
